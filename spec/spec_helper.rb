ee0dc6c1ba1e5665fea674ba595c1950b0ca8422
Matthew Beale <matt.beale@madhatted.com>
2011-02-18 11:51:18 -0500

    Clean up some of this searchd management madness

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,21 +11,50 @@ RSpec.configure do |config|
   config.mock_with :mocha
   config.use_transactional_fixtures = true
   config.before :all do
-    config = YAML.load(ERB.new(IO.read(File.join(File.dirname(__FILE__), 'support/database.yml'))).result)['test']
-    generator = Thebes::ConfigWriter.new(File.join(File.dirname(__FILE__), 'support'), 'test.sphinx.conf')
-    generator.build File.join(File.dirname(__FILE__), 'support/test.sphinx.conf'), config
 
-    riddle_config = Riddle::Configuration.new
-    riddle_config.searchd.pid_file = 'tmp/searchd.pid'
-    riddle_config.searchd.log = 'tmp/searchd.log'
+    # Fetch a database configuration.
+    #
+    db_config = YAML.load( ERB.new( IO.read(
+      File.join(File.dirname(__FILE__), 'support/database.yml')
+    ) ).result )['test']
+
+    # Write a sphinx configuation for test mode.
+    #
+    generator = Thebes::ConfigWriter.new \
+      File.join(File.dirname(__FILE__), 'support'),
+      'test.sphinx.conf'
+    generator.build \
+      File.join(File.dirname(__FILE__), 'support/test.sphinx.conf'),
+      db_config
+
+    # Give Riddle a dummy configuration so we can use it to
+    # control searchd.
+    #
+    r_config = Riddle::Configuration.new
+    r_config.searchd.pid_file = 'tmp/searchd.pid'
+    r_config.searchd.log = 'tmp/searchd.log'
+
+    # Create a Riddle controller.
+    #
     @sphinx = Riddle::Controller.new \
-      riddle_config,
+      r_config,
       File.join(File.dirname(__FILE__), 'support/test.sphinx.conf')
-    @sphinx.start
+
+    # Build an initial index.
+    #
     @sphinx.index
+
+    # Start searchd.
+    #
+    @sphinx.start
+
   end
   config.after :all do
+
+    # Stop searchd after specs run.
+    #
     @sphinx.stop
+
   end
 end
 
